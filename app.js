711d6b0cd8f5753a939f71ce3f27425341ff42a1
PedroSantanass <50208150+PedroSantanass@users.noreply.github.com>
2019-10-31 12:04:35 -0300

    4.1

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-let firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "project-id.firebaseapp.com",
-    databaseURL: "https://project-id.firebaseio.com",
-    projectId: "project-id",
-    storageBucket: "project-id.appspot.com",
-    messagingSenderId: "sender-id",
-    appID: "app-id",
-  }
+var firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appID: "app-id",
+};
+firebase.initializeApp(firebaseConfig);
