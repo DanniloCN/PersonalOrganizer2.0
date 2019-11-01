e6c7adcb9c96dd9520a464ef498f88ad9ae14b20
Pedro Santana <ap911152@gmail.com>
2019-11-01 10:16:20 -0300

    Traduzido/Atualizado parte do FireBase

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDgxrvLyYnUFKBUoZ4pHzOD2QbW_3i8AF8",
+    authDomain: "personalorganizersmatch.firebaseapp.com",
+    databaseURL: "https://personalorganizersmatch.firebaseio.com",
+    projectId: "personalorganizersmatch",
+    storageBucket: "personalorganizersmatch.appspot.com",
+    messagingSenderId: "929422065702",
+    appId: "1:929422065702:web:5d6d162f5eea24442fe6e7"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
