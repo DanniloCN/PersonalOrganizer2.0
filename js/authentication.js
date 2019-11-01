726c66b92e88f59153f77b75c7a5007907924484
Dannilo <dannilocarlos@gmail.com>
2019-11-01 13:12:07 -0300

    BASE DE FOGO FEITO, CARALHO

--- js/authentication.js
+++ js/authentication.js
@@ -1,29 +1,30 @@
-var usersList = document.getElementById('usersList');
 var inputEmail = document.getElementById("inputEmail");
 var inputPassword = document.getElementById("inputPassword");
 var btnCriar = document.getElementById("btnCriar");
+var inputFirstName = document.getElementById("inputFirstName");
+var inputLastName = document.getElementById("inputLastName");
+
 
 
 btnCriar.addEventListener('click', function() {
-    create(inputEmail.value, inputPassword.value);
+    firebase
+        .auth()
+        .createUserWithEmailAndPassword(inputEmail.value, inputPassword.value)
+        .then(function () {
+            alert('Você foi cadastrado');
+            databaseName(inputFirstName.value, inputLastName.value )
+        })
+        .catch(function (error) {
+            console.log(error.code);
+            console.log(error.message);
+            alert('Houve algum erro, verifique o console');
+        })
 
     });
-
-    function create(email, password) {
-        var data = {
-            email: email,
-            password: password
-        };
-
-        return firebaseConfig.database().ref().child('users').push(data);
+    function databaseName(nome, sobrenome){
+        let data = {
+            nome: nome,
+            sobrenome: sobrenome
+        }
+        firebase.database().ref().child("user").push(data);
     }
-
-    firebaseConfig.database().ref('users').on('value', function (snapshot){
-        usersList.innerHTML = '';
-        snapshot.forEach(function (item) {
-            var li = document.createElement('li');
-            li.appendChild(document.createTextNode(item.val().email + ': ' + item.val().password));
-            usersList.appendChild(li);
-        });
- 
-    });
