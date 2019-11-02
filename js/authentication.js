612d2fe23eafaa2ed2358ca3bf41b7b8b637b450
DanniloCN <dannilocarlos@gmail.com>
2019-11-01 22:31:55 -0300

    Login começado, mas não terminado

--- js/authentication.js
+++ js/authentication.js
@@ -1,6 +1,7 @@
 var inputEmail = document.getElementById("inputEmail");
-var inputPassword = document.getElementById("inputPassword");
+var inputEmail = document.getElementById("inputPassword");
 var btnCriar = document.getElementById("btnCriar");
+var btnLogin = document.getElementById("btnLogin");
 var inputFirstName = document.getElementById("inputFirstName");
 var inputLastName = document.getElementById("inputLastName");
 
@@ -10,21 +11,38 @@ btnCriar.addEventListener('click', function() {
     firebase
         .auth()
         .createUserWithEmailAndPassword(inputEmail.value, inputPassword.value)
-        .then(function () {
+        .then(function() {
             alert('Você foi cadastrado');
-            databaseName(inputFirstName.value, inputLastName.value )
+            databaseName(inputFirstName.value, inputLastName.value)
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log(error.code);
             console.log(error.message);
             alert('Houve algum erro, verifique o console');
         })
 
-    });
-    function databaseName(nome, sobrenome){
-        let data = {
-            nome: nome,
-            sobrenome: sobrenome
-        }
-        firebase.database().ref().child("user").push(data);
+});
+
+function databaseName(nome, sobrenome) {
+    let data = {
+        nome: nome,
+        sobrenome: sobrenome
     }
+    firebase.database().ref().child("user").push(data);
+}
+
+btnLogin.addEventListener('click', function() {
+
+    firebase.auth().signInWithEmailAndPassword(inputEmail.value, inputEmail.value).then(function(result) {
+        alert("Usuário conectado!");
+
+    }).catch(function(error) {
+
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        alert(errorMessage);
+
+    });
+
+});
