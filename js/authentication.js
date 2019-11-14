3f3d443b8dcefdb2de0b65bc45424d7b589908bf
DanniloCN <dannilocarlos@gmail.com>
2019-11-13 22:36:15 -0300

    5.0
    Falta 1 tela e MATCH!

--- js/authentication.js
+++ js/authentication.js
@@ -5,26 +5,26 @@ var inputFirstName = document.getElementById("inputFirstName");
 var inputLastName = document.getElementById("inputLastName");
 
 
-
 btnCriar.addEventListener('click', function() {
     firebase
         .auth()
         .createUserWithEmailAndPassword(inputEmail.value, inputPassword.value)
-        .then(function () {
+        .then(function() {
             alert('Você foi cadastrado');
-            databaseName(inputFirstName.value, inputLastName.value )
+            databaseName(inputFirstName.value, inputLastName.value)
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.log(error.code);
             console.log(error.message);
             alert('Houve algum erro, verifique o console');
         })
 
-    });
-    function databaseName(nome, sobrenome){
-        let data = {
-            nome: nome,
-            sobrenome: sobrenome
-        }
-        firebase.database().ref().child("user").push(data);
+});
+
+function databaseName(nome, sobrenome) {
+    let data = {
+        nome: nome,
+        sobrenome: sobrenome
     }
+    firebase.database().ref().child("user").push(data);
+}
