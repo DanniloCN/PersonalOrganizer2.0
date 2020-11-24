85c498034a25d82a2c7452ed672a31d0c4e152f9
Dannilo Carlos <dannilocarlos@gmail.com>
2020-11-24 07:45:10 -0300

    Revert "Merge pull request #2 from DanniloCN/test"
    This reverts commit b329d3ae93310407ab6a85467ce73f35bf0c7edc, reversing
    changes made to cc1912e889ec65cf05720a8eb03341f8d119c536.

--- js/authentication.js
+++ js/authentication.js
@@ -3,13 +3,12 @@ var inputPassword = document.getElementById("inputPassword");
 var btnCriar = document.getElementById("btnCriar");
 var inputFirstName = document.getElementById("inputFirstName");
 var inputLastName = document.getElementById("inputLastName");
-var tipoUsuario = document.getElementById("tipoUsuario")
 
 
 btnCriar.addEventListener('click', function() {
     firebase
         .auth()
-        .createUserWithEmailAndPassword(inputEmail.value, inputPassword.value, tipoUsuario.value)
+        .createUserWithEmailAndPassword(inputEmail.value, inputPassword.value)
         .then(function() {
             alert('Você foi cadastrado');
             databaseName(inputFirstName.value, inputLastName.value)
@@ -17,17 +16,15 @@ btnCriar.addEventListener('click', function() {
         .catch(function(error) {
             console.log(error.code);
             console.log(error.message);
-            alert('Houve algum erro, o e-mail já foi utilizado.');
+            alert('Houve algum erro, verifique o console');
         })
 
 });
 
-function databaseName(nome, sobrenome, tipoUsuario) {
+function databaseName(nome, sobrenome) {
     let data = {
         nome: nome,
-        sobrenome: sobrenome,
-        tipoUsuario: tipoUsuario
-        
+        sobrenome: sobrenome
     }
     firebase.database().ref().child("user").push(data);
 }
